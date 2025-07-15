936ae375699091a3f7e183229f1bd0cbb45233d1
SeongJongHo <whdgh9595@gmail.com>
2025-07-16 02:16:40 +0900

    ADD: 회원가입 api 호출 로직 추가

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,4 +1,4 @@
-import type { LoginCredentials } from "@/entities/user";
+import type { LoginCredentials, SignupRequest } from "@/entities/user";
 import { apiClient } from "@/shared/lib/api/apiClient";
 
 export const authApi = {
@@ -19,6 +19,21 @@ export const authApi = {
             }
 
             throw new Error("로그인 중 오류가 발생했습니다.");
+        }
+    },
+
+    signup: async (data: SignupRequest): Promise<void> => {
+        try {
+            await apiClient.post("/api/v1/auth/sign-up", data);
+        } catch (error: any) {
+            if (error.response?.status) {
+                throw new Error(
+                    error.response?.message ||
+                        "회원가입 정보가 올바르지 않습니다."
+                );
+            }
+
+            throw new Error("회원가입 중 오류가 발생했습니다.");
         }
     },
 };
