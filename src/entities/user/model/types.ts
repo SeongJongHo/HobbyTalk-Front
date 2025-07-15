00126721c93dcb303f7942f1687b9415c26a5bd3
SeongJongHo <whdgh9595@gmail.com>
2025-07-16 01:57:22 +0900

    ADD: 회원가입 데이터 타입 정의

--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -2,3 +2,20 @@ export type LoginCredentials = {
     username: string;
     password: string;
 };
+
+export const UserRole = {
+    USER: "USER",
+    ADMIN: "ADMIN",
+} as const;
+
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export type SignupRequest = {
+    nickname: string;
+    password: string;
+    password_confirm: string;
+    username: string;
+    phone_number: string;
+    profile_image?: string | null;
+    role?: UserRole;
+};
