aee47138bb470ddc79b9cb8b01dad14b5b936e33
SeongJongHo <whdgh9595@gmail.com>
2025-07-16 22:21:44 +0900

    MODIFY: 토큰 저장 로직 변경

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,40 +1,40 @@
-import type { LoginCredentials, SignupRequest } from "@/entities/user";
-import { apiClient } from "@/shared/lib/api/apiClient";
+import type { LoginCredentials, SignupRequest } from '@/entities/user';
+import { apiClient } from '@/shared/lib/api/apiClient';
 
 export const authApi = {
     login: async (credentials: LoginCredentials): Promise<string> => {
         try {
             const response = await apiClient.post(
-                "/api/command/v1/auth/sign-in",
+                '/api/command/v1/auth/sign-in',
                 credentials
             );
 
-            return response.data.token;
+            return response.data.data.access_token;
         } catch (error: any) {
             if (error.response?.data?.status) {
-                console.error("Login error:", error);
+                console.error('Login error:', error);
                 throw new Error(
                     error.response?.data?.message ||
-                        "로그인 정보가 올바르지 않습니다."
+                        '로그인 정보가 올바르지 않습니다.'
                 );
             }
 
-            throw new Error("로그인 중 오류가 발생했습니다.");
+            throw new Error('로그인 중 오류가 발생했습니다.');
         }
     },
 
     signup: async (data: SignupRequest): Promise<void> => {
         try {
-            await apiClient.post("/api/command/v1/auth/sign-up", data);
+            await apiClient.post('/api/command/v1/auth/sign-up', data);
         } catch (error: any) {
             if (error.response?.data?.status) {
                 throw new Error(
                     error.response?.data?.message ||
-                        "회원가입 정보가 올바르지 않습니다."
+                        '회원가입 정보가 올바르지 않습니다.'
                 );
             }
 
-            throw new Error("회원가입 중 오류가 발생했습니다.");
+            throw new Error('회원가입 중 오류가 발생했습니다.');
         }
     },
 };
