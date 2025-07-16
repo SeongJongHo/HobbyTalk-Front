a1c034b8b37b4a9f7ffe04b756c9be8740c35c7c
SeongJongHo <whdgh9595@gmail.com>
2025-07-16 21:32:56 +0900

    CHORE: 프리티어 설정 파일 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
+    const env = loadEnv(mode, process.cwd(), '');
 
     console.log(
         `\x1b[36m%s\x1b[0m`,
@@ -12,15 +12,18 @@ export default defineConfig(({ mode }) => {
 
     return {
         plugins: [react()],
+        build: {
+            sourcemap: false, // Source Map 비활성화
+        },
         resolve: {
             alias: {
-                "@": path.resolve(__dirname, "./src"),
-                "@/app": path.resolve(__dirname, "./src/app"),
-                "@/pages": path.resolve(__dirname, "./src/pages"),
-                "@/widgets": path.resolve(__dirname, "./src/widgets"),
-                "@/features": path.resolve(__dirname, "./src/features"),
-                "@/entities": path.resolve(__dirname, "./src/entities"),
-                "@/shared": path.resolve(__dirname, "./src/shared"),
+                '@': path.resolve(__dirname, './src'),
+                '@/app': path.resolve(__dirname, './src/app'),
+                '@/pages': path.resolve(__dirname, './src/pages'),
+                '@/widgets': path.resolve(__dirname, './src/widgets'),
+                '@/features': path.resolve(__dirname, './src/features'),
+                '@/entities': path.resolve(__dirname, './src/entities'),
+                '@/shared': path.resolve(__dirname, './src/shared'),
             },
         },
     };
