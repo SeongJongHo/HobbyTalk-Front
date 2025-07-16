e481ce97a0a66dd3403b5dbf267444da8e12c514
SeongJongHo <whdgh9595@gmail.com>
2025-07-17 03:10:51 +0900

    ADD: 내 채팅방 타입 정의

--- src/entities/chatRoom/model/types.ts
+++ src/entities/chatRoom/model/types.ts
@@ -7,3 +7,14 @@ export interface ChatRoom {
     created_at: string;
     is_private: boolean;
 }
+
+export type MyChatRoom = {
+    id: number;
+    title: string;
+    maximum_capacity: number;
+    current_attendance: number;
+    category: string;
+    created_at: string;
+    is_private: boolean;
+    unread_count: number;
+};
